6002deee1b67df5dc0fb666f560a57b970119824
calint <calin.tenitchi@gmail.com>
2025-12-02 19:45:21 +0100

    .

--- program.s
+++ program.s
@@ -1,47 +1,59 @@
 .export start
 
 VIC_CONTROL_X = $D016  ; VIC-II Screen Control Register 2 (Horizontal Scroll)
-SCROLL_VAL    = $FE    ; Use $FE for our Page Zero variable
 SCREEN        = $0400  ; screen address
+SCROLL_X      = $FE
+DELAY1        = $FD
+DELAY2        = $FC
+MAP_OFFSET_X  = $FB
 
 start:
-    lda #$00
-    sta SCROLL_VAL
+    lda #7
+    sta SCROLL_X
+    lda #0
+    sta MAP_OFFSET_X
 
-    ldx #$00           ; index into string
+print_screen:
+    ldx MAP_OFFSET_X
+    ldy #0
+
 loop1:
     lda message, x
-    beq scroll         ; message ends with a 0-byte
-    sta SCREEN, x      ; copy character code to screen memory ($0400 is the default)
+    beq scroll_left
+    sta SCREEN, y
     inx
+    iny
     jmp loop1
 
-scroll:
-    lda SCROLL_VAL
-    cmp #$08           ; check if it has reached 8 (finished the 8-pixel cycle)
-    bne fine_scroll    ; if not 8, do fine scrolling
-    lda #$00
-    sta SCROLL_VAL
+scroll_left:
+    lda SCROLL_X
+    cmp #255
+    bne fine_scroll
+    lda #7
+    sta SCROLL_X
+    inc MAP_OFFSET_X
+    jmp print_screen
 
 fine_scroll:
-    lda VIC_CONTROL_X  ; Load current VIC-II control bits (e.g., 40-column mode)
-    and #%11111000     ; Mask off the old scroll value (bits 0, 1, 2)
-    ora SCROLL_VAL     ; OR with the new value from Page Zero (0-7)
-    sta VIC_CONTROL_X  ; Set the new scroll position
-    inc SCROLL_VAL     ; Increment $FE
+    lda VIC_CONTROL_X
+    and #%11111000
+    ora SCROLL_X
+    sta VIC_CONTROL_X
+    dec SCROLL_X
     jsr delay
-    jmp loop1          ; Jump to the waiting loop (to simulate a delay or frame wait)
+    jmp scroll_left
 
 delay:
-    ldx #0
-    ldy #0
+    lda #0
+    sta DELAY1
+    sta DELAY2
 delay1:
-    dex
+    dec DELAY1
     bne delay1
-    dey
+    dec DELAY2
     bne delay1
     rts
 
 message:
         ; 8,5,12,12,15 (HELLO) 32 (SPACE) 23,15,18,12,4 (WORLD) 0 (terminator)
-        .byte 8,5,12,12,15,32,23,15,18,12,4,0
+        .byte 32,32,32,8,5,12,12,15,32,23,15,18,12,4,32,32,32,0
