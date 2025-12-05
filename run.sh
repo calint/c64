64d5028c58e1e625f9ac58d8ac2f0613bce943b0
calint <calin.tenitchi@gmail.com>
2025-12-05 20:44:11 +0100

    .

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
+cd "$(dirname "$0")"
 
+resources/update.sh
 cl65 -C c64-asm.cfg -u __EXEHDR__ -o program.prg src/program.s
 x64 -silent -windowwidth 1920 -windowheight 1360 program.prg
