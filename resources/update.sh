af783d6033a72053a2065ed69998fbe5d0fdf0fe
calint <calin.tenitchi@gmail.com>
2025-12-10 11:44:56 +0100

    .

--- resources/update.sh
+++ resources/update.sh
@@ -2,11 +2,10 @@
 set -e
 cd $(dirname "$0")
 
-SIZE=${1:-25}
-ROWS=${2:-25}
-START_ROW=${3:-0}
+RENDER_START_ROW=0
+RENDER_N_ROWS=25
 
-./gen-tile-map.py tile-map.tmx $SIZE 0xfffffff >../src/tile_map.s
-./gen-render-to-screen.py screen_0 $START_ROW $ROWS >../src/render_to_screen_0.s
-./gen-render-to-screen.py screen_1 $START_ROW $ROWS >../src/render_to_screen_1.s
+./gen-tile-map.py tile-map.tmx 0 0xfffffff >../src/tile_map.s
+./gen-render-to-screen.py screen_0 $RENDER_START_ROW $RENDER_N_ROWS >../src/render_to_screen_0.s
+./gen-render-to-screen.py screen_1 $RENDER_START_ROW $RENDER_N_ROWS >../src/render_to_screen_1.s
 ./gen-charset.py 8 8 custom-charset.png >../src/charset_2.s
