951a2dc04a1f1ef93a05ac6575ec370334644aac
calint <calin.tenitchi@gmail.com>
2025-12-20 10:56:01 +0100

    .

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 set CC65_BIN=..\cc65\bin
 set VICE_PATH=..\SDL2VICE-3.9-win64
 
-,,\resources\update.bat
+..\resources\update.bat
 %CC65_BIN%\cl65.exe -C linker.cfg -o program.prg src\main.s
 %VICE_PATH%\x64sc.exe -silent -windowwidth 1024 -windowheight 768 program.prg
