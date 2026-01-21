8ab3688ebc6c630a241c6939baa09fdb7fe1c378
calint <calin.tenitchi@gmail.com>
2026-01-21 20:20:56 +0100

    .

--- src/main.s
+++ src/main.s
@@ -1306,15 +1306,15 @@ render_tile_map:
  
     ; unrolled loops uses ~10,075 cycles vs ~18,725 cycles using loops
 
-    .repeat 25, row
+    .repeat 25, ROW
         ldx tmp1
-        .repeat SCREEN_WIDTH - 1, col
-            lda tile_map + row * TILE_MAP_WIDTH, x
-            sta screen + row * SCREEN_WIDTH + col
+        .repeat SCREEN_WIDTH - 1, COL
+            lda tile_map + ROW * TILE_MAP_WIDTH, x
+            sta screen + ROW * SCREEN_WIDTH + COL
             inx
         .endrepeat
-        lda tile_map + row * TILE_MAP_WIDTH, x
-        sta screen + row * SCREEN_WIDTH + SCREEN_WIDTH - 1
+        lda tile_map + ROW * TILE_MAP_WIDTH, x
+        sta screen + ROW * SCREEN_WIDTH + SCREEN_WIDTH - 1
     .endrepeat
 
     ; restore border color
